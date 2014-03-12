8f9db1b899d187bc940235bed333dfb3f9f14d45
a577526 <a577526@MC0W6ASC.ww930.my-it-solutions.net>
2014-03-12 16:58:33 +0100

    commit

---
+++ src/main/java/com/mycompany/mavenprojectjava/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.mavenprojectjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
